0ab0b322a46c85ffe559d59e1a06ba917eb750c1
ValerioScharizza <valerio.schiarizza@live.it>
2022-08-24 14:56:15 +0200

    Created the Programm

--- js/script.js
+++ js/script.js
@@ -1,1 +1,43 @@
 console.log("JS OK!")
+
+const kmInput = document.getElementById("km_input");
+const ageInput = document.getElementById("age_input");
+
+const ticketGeneratorButton = document.getElementById("ticket_generator");
+
+ticketGeneratorButton.addEventListener("click",
+    function () {
+        const kmValue = parseInt(kmInput.value);
+        const ageValue = parseInt(ageInput.value);
+        let ticketPrice = (kmValue * 0.21);
+        if (!isNaN(kmValue) && !isNaN(ageValue)) {
+            let ticketPriceCurrent = 0;
+            if (ageValue < 18) {
+                ticketPriceCurrent = ticketPrice * 4 / 5;
+            } else if (ageValue > 64) {
+                ticketPriceCurrent = ticketPrice * 3 / 5;
+            } else {
+                ticketPriceCurrent = ticketPrice;
+            }
+            document.getElementById("ticket").innerHTML = ticketPriceCurrent.toFixed(2) + " Euro";
+        }
+        else {
+            document.getElementById("ticket").innerHTML = "Non hai inserito un numero!";
+        }
+    }
+)
+
+// if (!isNaN(km) && !isNaN(age)) {
+//     let ticketPriceCurrent = 0;
+//     if (age < 18) {
+//         ticketPriceCurrent = ticketPrice * 4 / 5;
+//     } else if (age > 64) {
+//         ticketPriceCurrent = ticketPrice * 3 / 5;
+//     } else {
+//         ticketPriceCurrent = ticketPrice;
+//     }
+//     document.getElementById("price").innerHTML = ticketPriceCurrent.toFixed(2) + " Euro";
+// }
+// else {
+//     document.getElementById("price").innerHTML = "Non hai inserito un numero!";
+// }
